0a6699b7e5d1b615cb6697f15582c2ef71b69350
Andrea Abellera <58119036+andreaabellera@users.noreply.github.com>
2022-04-14 04:36:38 -0500

    Ready for ajaxifying

--- create.js
+++ create.js
@@ -1,31 +1,34 @@
 // Takes an artwork name and the div container to place it in
 function create(container,name){
-    let artLoc = artLib[name]
-
-    const req = new XMLHttpRequest();
-    req.onreadystatechange=function(){
-      if (req.readyState==4 && req.status==200){
-        // Constrain div to square (for now)
-        let dimensions = [container.clientHeight,container.clientWidth]
-        if(dimensions[0] < dimensions[1]){ // landscape
-            container.style.height = dimensions[0] + "px"
-            container.style.width = dimensions[0] + "px"
-        }
-        else if(dimensions[0] > dimensions[1]){ // portrait
-            container.style.height = dimensions[1] + "px"
-            container.style.width = dimensions[1] + "px"
-        }
-        console.log(container.clientHeight)
-        container.innerHTML = req.responseText
-        return req.responseText
-      }
+  const req1 = new XMLHttpRequest();
+  req1.onreadystatechange=function(){
+    if (req1.readyState==4 && req1.status==200){
+      html = this.responseText
+      container.innerHTML = html;
     }
-    req.open("GET", artLoc, true)
-    req.send();
-}
+  }
+  req1.open("GET", name + "/" + name + ".txt", true);
+  req1.send();
 
-const artLib = {
-    "orange": "orange/orange.txt"
+  const req2 = new XMLHttpRequest();
+  req2.onreadystatechange=function(){
+  if (req2.readyState==4 && req2.status==200){
+      css = this.responseText
+      let container = document.getElementById("meta-container")
+      let dimensions = [container.clientHeight,container.clientWidth]
+      if(dimensions[0] < dimensions[1]){ // landscape
+          container.style.height = dimensions[0] + "px"
+          container.style.width = dimensions[0] + "px"
+      }
+      else if(dimensions[0] > dimensions[1]){ // portrait
+          container.style.height = dimensions[1] + "px"
+          container.style.width = dimensions[1] + "px"
+      }
+      container.innerHTML += `<style> ${css} </style>`
+    }
+  }
+  req2.open("GET", name + "/" + name + ".css",true)
+  req2.send()
 }
 
 module.exports = create
