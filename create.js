3348a133f57699a9aace184748a3944de866b11a
Andrea Abellera <andrea.abellera@gmail.com>
2022-06-10 09:45:18 -0500

    Casually Mad lab testing: Sweep 2
    Specimens in passable quality. Matcha wood defect. Octopus out of control,  needs containment
    
    create.js replasters art on window resize for responsivity

--- create.js
+++ create.js
@@ -11,7 +11,9 @@
    
 */
 
-function create(container,name){
+let repertoire = []
+
+function create(container,name,refresh=false){
   ;(async () => {
     let html = await load(name + "/" + name + ".txt", container)
     let css = await load(name + "/" + name + ".css", container, true)
@@ -27,6 +29,10 @@ function create(container,name){
       let scale = Math.min(targetHeight/artboardHeight, targetWidth/artboardWidth)
       artboard.style.transform = "scale(" + scale + ")"
       artboard.style.transformOrigin = "0 0"
+
+      // Add to repertoire
+      if(!refresh)
+        repertoire.push({ container: container, content: container.innerHTML, name: name })
     }
     else
       console.error(`Casually CSS: Artwork load failed. Please check that your '${name}' parameter is of string type and exists in Casually CSS. If it exists, folder for '${name}' may be missing. Please redownload the '${name}' folder from 'https://github.com/andreaabellera/Casually-CSS' or reinstall the package.`)
@@ -43,6 +49,27 @@ async function load(url, container, css=false){
     container.innerHTML += text
 
   return response.ok;
+}
+
+window.onresize = function(){
+  repertoire.forEach(art => { 
+    art.container.innerHTML = ""
+    replaster(art.container, art.content)
+  })
+}
+
+function replaster(container, content){
+  let targetHeight = container.clientHeight
+  let targetWidth = container.clientWidth
+  container.innerHTML = content
+  let artboard = container.getElementsByClassName("artboard")[0]
+  let artboardHeight = artboard.clientHeight
+  let artboardWidth = artboard.clientWidth
+
+  // Resize artboard to fit container
+  let scale = Math.min(targetHeight/artboardHeight, targetWidth/artboardWidth)
+  artboard.style.transform = "scale(" + scale + ")"
+  artboard.style.transformOrigin = "0 0"
 }
 
 module.exports = create
