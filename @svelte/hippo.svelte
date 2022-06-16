f878343d2e5f374c6b1490498ac00f86d020d295
Andrea Abellera <andrea.abellera@gmail.com>
2022-06-15 23:09:23 -0500

    Bison, hippo: Add uuid generation for svelte
    Bug discovered
    Will do this to all artworks before patch update

--- @svelte/hippo.svelte
+++ @svelte/hippo.svelte
@@ -24,9 +24,10 @@
     delay: "0s",
     duration: "10s"
   }
+  window.genId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 </script>
 
-<div class="artboard">
+<div class="artboard" id={window.genId}>
   <div id="hippo">
     <div class="hippo">
         <span class="hippo-bod" style="
@@ -119,7 +120,7 @@
     // Resize artboard to fit container
     resize()
     function resize(){
-      let artboard = document.getElementsByClassName("artboard")[0]
+      let artboard = document.getElementById(window.genId)
       let targetHeight = artboard.parentElement.clientHeight
       let targetWidth = artboard.parentElement.clientWidth
       let artboardHeight = artboard.clientHeight
